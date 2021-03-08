028ec4ba045b4c3a79da15b97099054dcecd8a12
Jason Fang <jasonvfang@126.com>
2021-03-08 23:47:22 +0800

    Add remove duplicates from sorted list.2.

--- Leetcode/81.RemoveDuplicatesFromSortedListsII/remove_duplicates_from_sortedList.c
+++ Leetcode/81.RemoveDuplicatesFromSortedListsII/remove_duplicates_from_sortedList.c
@@ -24,8 +24,12 @@ int ListNode_init(struct ListNode *Head, int listNum, int len)
 
         tmp->next = NULL;
         last->next = tmp;
-        if (i >= 3 && i <= 5)
+        if (i <= 2)
+            tmp->val = 1;
+        else if (i >= 3 && i <= 5)
             tmp->val = 4;
+        else if (i >=8)
+            tmp->val = 8;
         else
             tmp->val = i + 1;
         last = tmp;
@@ -78,117 +82,65 @@ void print_list_noheader(struct ListNode *list)
 }
 
 
-struct ListNode* rotateRight(struct ListNode* head, int k)
-{
-    if (head && k > 0)
-    {
-        struct ListNode* pTmp = head->next;
-        struct ListNode* pPrev = head;
-        struct ListNode* pheadAnchor = head;
-        int length = 0;
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    if (head == NULL)
+    	return NULL;
+    
+	struct ListNode* p = NULL, *q = NULL, *pprev = NULL;
+	
+	p = head;
+	q = p->next;
+    pprev = p;
 
-        //Find the last node.
-        while (pTmp)
+	while(p)
+	{
+        if (p && q && p->val == q->val)
         {
-            length ++;
-            if (NULL == pTmp->next)
+            struct ListNode*tmp = NULL;
+            while (q && p->val == q->val)
             {
-                length ++;
-                break;
+                tmp = q->next;                
+                free(q);
+                q = tmp;
             }
+            
+            if (p == pprev)//head node
+            {
+                pprev = tmp;
+                head = pprev;
+            }else{
+                pprev->next = tmp;
+            }
+            
+            free(p);
+            p = tmp;
+            
+            if (p)
+                q = p->next;
+        }
+        else
+        {
+            pprev = p;
+            p = p->next;
+
+            if (p == NULL || p->next == NULL)
+                break;
            
-            pPrev = pTmp;
-            pTmp = pTmp->next;
+            q = p->next;
         }
-
-        if (pTmp == NULL && pPrev == pheadAnchor)
-        {
-            //only one node
-            return head;
-        }
-
-        k = k % length;
-
-        //Do rotate and starts with the last node.
-        while (k --)
-        {
-            pTmp->next = pheadAnchor;
-            pheadAnchor = pTmp;
-            pPrev->next = NULL;
-
-            struct ListNode* p = pheadAnchor;
-            while (p)
-            {
-                if (p->next == pPrev)
-                {
-                    break;
-                }
-
-                p = p->next;
-            }
-
-            pTmp = pPrev;
-            pPrev = p;
-        }
-
-        head = pheadAnchor;
-    }
-
-    return head;
+	}
+	
+	return head;
 }
-
-struct ListNode* rotateRight2(struct ListNode* head, int k)
-{
-    if (head && k > 0)
-    {
-        struct ListNode* pTail = head;
-        struct ListNode* pCurr = head;
-        int length = 0, count = 0;
-
-        //Find the last node.
-        while (pTail)
-        {
-            if (NULL == pTail->next)
-            {
-                length ++;
-                break;
-            }
-
-            length ++;
-            pTail = pTail->next;
-        }
-
-        if (length == 1 || k == length)
-        {
-            return head;
-        }
-
-        k = k % length;
-        count = length - (k + 1);
-
-        while (count --)
-        {
-            pCurr = pCurr->next;
-        }
-
-        pTail->next = head;
-        head = pCurr->next;
-        pCurr->next = NULL;
-    }
-
-    return head;
-}
-
 
 int main(int argc, char *argv[])
 {
-    ListNode_init(&ListHead_1, 1, 7);
+    ListNode_init(&ListHead_1, 1,11);
 
     printf("List:\n");
     print_list(&ListHead_1);
-    int k = atoi(argv[1]);
 
-    struct ListNode *newList = rotateRight2(ListHead_1.next, k);
+    struct ListNode *newList = deleteDuplicates(ListHead_1.next);
 
     printf("New List:\n");
     print_list_noheader(newList);
